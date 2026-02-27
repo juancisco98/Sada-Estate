e891d10025202488896ff296746040ebd39a8449
juancisco98 <juan.sada98gmail.com>
2026-02-27 20:48:10 +0100

    feat: add capacitor to build android native app

---
+++ android/app/src/main/java/com/svpropiedades/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.svpropiedades.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.svpropiedades.app',
+  appName: 'SV Propiedades',
+  webDir: 'dist'
+};
+
+export default config;
