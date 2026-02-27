6b5b35ec843fade3f95f947c60136fbd7552f4d2
juancisco98 <juan.sada98gmail.com>
2026-02-27 20:37:39 +0100

    fix: pwa config

--- index.tsx
+++ index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { registerSW } from 'virtual:pwa-register';
+
+if ('serviceWorker' in navigator) {
+  registerSW({ immediate: true });
+}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
